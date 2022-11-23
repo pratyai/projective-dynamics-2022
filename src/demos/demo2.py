72c54d90c9ed191b3f4e70aa50eaaa4dacc55e35
Tal Rastopchin <rastopch@grinnell.edu>
2022-11-23 12:05:36 +0100

    massively restructure demo2.py; use argparse to properly handle command line arguments; keep track of the mesh and system in the state dictionary; refactor initializing and updating the system and polyscope state into separate functions; polish ui

--- src/demos/demo2.py
+++ src/demos/demo2.py
@@ -1,36 +1,96 @@
+# Library imports
+import argparse
 import numpy as np
 import polyscope as ps
 import polyscope.imgui as psim
+import openmesh as om
+
+# Our imports
 import system as msys
-from . import demo_systems as demos
-import sys
+from . import demo_systems
 
-
-def ui_callback(state: dict, system):
-    '''
-    `state` is a dictionary of ui states to be updated. It is a dictionary now for flexibility.
-    '''
-    button_text = "Start" if not state['ui_is_running'] else "Stop"
-    if (psim.Button(button_text)):
-        state['ui_is_running'] = not state['ui_is_running']
-    changed, state['ui_h'] = psim.SliderFloat(
-        "Step size", state['ui_h'], v_min=0.001, v_max=1.0)
-    changed, state['ui_steps_per_frame'] = psim.InputInt(
-        "ui_int", state['ui_steps_per_frame'], step=1, step_fast=10)
-    if 'ps_mesh' not in state:
-        def comp_line_segs(): return np.array([[qi, c.p0()[1]]
-                                               for (qi, (q, cs)) in enumerate(zip(system.q, system.cons))
-                                               for c in cs
-                                               if c.p0()[1] is not None])
-        state['ps_mesh'] = ps.register_curve_network(
-            name='ps_mesh', nodes=system.q, edges=comp_line_segs())
+def ui_callback(state: dict):
+    """
+    The callback function for the Polyscope application. Manages the user interface, the polyscope application, and the system.
+    """
+    
+    # System parameters section
+    if(psim.TreeNodeEx("System Parameters",
+                       flags=psim.ImGuiTreeNodeFlags_DefaultOpen)):
+    
+        # Reset simulation button
+        if (psim.Button("Reset")):
+            state['ui_is_running'] = False
+            _initialize_system(state)
+            _update_polyscope_mesh(state)
+            
+        psim.TreePop()
+    
+    # System control section
+    if (psim.TreeNodeEx("Simulation Control",
+                       flags=psim.ImGuiTreeNodeFlags_DefaultOpen)):
+        
+        # Start / Stop simulation button
+        start_stop_button_text = "Start" if not state['ui_is_running'] else "Stop"
+        if (psim.Button(start_stop_button_text)):
+            state['ui_is_running'] = not state['ui_is_running']
+        
+        # Step size slider
+        changed, state['ui_h'] = psim.SliderFloat(
+            "Step size", state['ui_h'], v_min=0.001, v_max=1.0)
+        
+        # Steps per frame
+        changed, state['ui_steps_per_frame'] = psim.InputInt(
+            "ui_int", state['ui_steps_per_frame'], step=1, step_fast=10)
+    
+    # Update the system and polyscope mesh when the application is running
     if state['ui_is_running']:
-        for i in range(state['ui_steps_per_frame']):
-            system.step(state['ui_h'])
-        state['ps_mesh'].update_node_positions(system.q)
+        _update_system(state)
+        _update_polyscope_mesh(state)
 
+def _initialize_system(state: dict):
+    """
+    Initialize the system with specified point mass and spring stiffness
+    """
+    state['system'] = demo_systems.make_triangle_mesh_system(
+        state['mesh'], state['ui_point_mass'], state['ui_spring_stiffness'])
 
-def main(s: msys.System):
+def _update_system(state: dict):
+    """
+    Update our system by taking the specified amount of steps with specified step size.
+    """
+    for i in range(state['ui_steps_per_frame']):
+            state['system'].step(state['ui_h'])
+
+def _initialize_polyscope_mesh(state: dict):
+    """
+    Initialize the polyscope mesh to be a curve network where each edge corresponds to a spring in our system.
+    """
+    system = state['system']
+    edges = np.array([[qi, c.p0()[1]] 
+                      for (qi, (q, cs)) in enumerate(zip(system.q, system.cons))
+                      for c in cs
+                      if c.p0()[1] is not None])
+    state['ps_mesh'] = ps.register_curve_network(
+            name='ps_mesh', nodes=system.q, edges=edges)
+
+def _update_polyscope_mesh(state:dict):
+    """
+    Update the polyscope curve network according to the current vertex positions of the system.
+    """
+    state['ps_mesh'].update_node_positions(state['system'].q)
+
+def main(filename: str):
+    """
+    Creates a Polyscope application visualizing a mass-spring system simulated using projective dynamics.
+    
+    Parameters:
+        filename (str): filename of the triangle mesh used to initialize the mass-spring system
+    """
+    
+    # Read the mesh
+    mesh = om.read_trimesh(filename)
+    
     # Set the application name
     ps.set_program_name('Projective Dynamics Demo')
 
@@ -41,24 +101,45 @@ def main(s: msys.System):
     ps.set_up_dir('z_up')
     ps.set_ground_plane_mode('none')
 
+    '''
+    `state` is a dictionary representing the application state.
+    '''
     state = {
+        'mesh' : mesh,
+        'system' : None,
+        'ui_point_mass' : 1,
+        'ui_spring_stiffness' : 1,
+        'ps_mesh' : None,
         'ui_is_running': False,
         'ui_h': 0.01,
         'ui_steps_per_frame': 10
     }
-    ps.set_user_callback(lambda: ui_callback(state, s))
+    # Initialize the system and polysope mesh
+    _initialize_system(state)
+    _initialize_polyscope_mesh(state)
+    
+    # Set the user callback
+    ps.set_user_callback(lambda: ui_callback(state))
 
     # Pass control flow to polyscope, displaying the interactive window
     ps.show()
 
 
 if __name__ == '__main__':
-    # Simple command line argument handling
-    if (len(sys.argv) != 2):
-        print("Usage: python demo3.py mesh.obj")
-        exit()
+    """
+    Parse command line arguments and run the Polyscope application.
+    """
+    
+    # Parse our command line arguments
+    parser = argparse.ArgumentParser()
+    
+    # Require a mesh filename as input
+    parser.add_argument("filename",
+                        help="the input mesh filename",
+                        type=str)
+    
+    # Parse the command line arguments
+    args = parser.parse_args()
 
-    # Create a system fom our triangle mesh
-    filename = sys.argv[1]
-    s = demos.make_triangle_mesh_system(filename, 1, 10)
-    main(s)
+    # Pass the filename to our main function
+    main(args.filename)
